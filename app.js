d9b208f2e38ce4bc7318660dec0a47534eb0614e
Reza <reza.amini.b@gmail.com>
2023-07-01 11:39:25 +0100

    - added a second delay transformation effect to be used for section child such as paragraphs. In this case the title of every section appears first and then followed by delayed transition of the the its paragraph.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const observer = new IntersectionObserver((entries) => {
-    
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-
-            entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove('show');
+  entries.forEach((entry) => {
+    const children = Array.from(entry.target.children);
+    children.forEach((child) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        if (child.classList.contains("hidden-delay")) {
+          child.classList.add("show");
         }
-
-
+      } else {
+        entry.target.classList.remove("show");
+        if (child.classList.contains("hidden-delay")) {
+          child.classList.remove("show");
+        }
+      }
     });
+  });
 });
 
-const sections = document.querySelectorAll('.hidden');
+const sections = document.querySelectorAll(".hidden");
 sections.forEach((element) => observer.observe(element));
